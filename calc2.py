07f1445c374e737a818fb51b5d1b5a75d5ce2b93
Tamim Sookoor <tamimsookoor@gmail.com>
2016-01-22 21:32:25 -0500

    New version of the calculator that can handle whitespace characters, multi-digit integers, and subtraction

--- calc2.py
+++ calc2.py
@@ -1,14 +1,13 @@
 # Token types
-#
 # EOF (end-of-file) token is used to indicate that
 # there is no more input left for lexical analysis
 INTEGER, PLUS, MINUS, EOF = 'INTEGER', 'PLUS', 'MINUS', 'EOF'
 
 class Token(object):
-    def __init__ (self, type, value):
-        # token type: INTEGER, PLUS, or EOF
+    def __init__(self, type, value):
+        # token type: INTEGER, PLUS, MINUS, or EOF
         self.type = type
-        # token value: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '+', or None
+        # token value: non-negative integer value, '+', '-', or None
         self.value = value
 
     def __str__(self):
@@ -26,64 +25,65 @@ class Token(object):
     def __repr__(self):
         return self.__str__()
 
-
 class Interpreter(object):
     def __init__(self, text):
-        # client string input, e.g. "3+5"
-        self.text = text.replace(" ", "")
+        # client string input, e.g. "3 + 5", "12 - 5", etc
+        self.text = text
         # self.pos is an index into self.text
         self.pos = 0
         # current token instance
         self.current_token = None
+        self.current_char = self.text[self.pos]
 
     def error(self):
         raise Exception('Error parsing input')
 
-    def get_number(self, text, current_char):
-        while self.pos + 1 < len(text) and text[self.pos + 1].isdigit():
-            current_char += text[self.pos + 1]
-            self.pos += 1
-        return current_char
+    def advance(self):
+        """Advance the 'pos' pointer and set the 'current_char' variable."""
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None # Indicates end of input
+        else:
+            self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self):
+        """Return a (multidigit) integer consumed from the input."""
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
 
     def get_next_token(self):
         """Lexical analyzer (also known as scanner or tokenizer)
 
         This method is responsible for breaking a sentence
-        apart into tokens. One token at a time.
+        apart into tokens.
         """
-        text = self.text
+        while self.current_char is not None:
 
-        # is self.pos index past the end of the self.text?
-        # if so, then return EOF token because there is no more
-        # input left to convert into tokens
-        if self.pos > len(text) - 1:
-            return Token(EOF, None)
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
 
-        # get a character at the position self.pos and decide
-        # what token to create based on the single character
-        current_char = text[self.pos]
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
 
-        # if the character is a digit then convert it to
-        # integer, create an INTEGER token, increment self.pos
-        # index to point to the next character after the digit,
-        # and return the INTEGER token
-        if current_char.isdigit():
-            current_char = self.get_number(text, current_char)
-            token = Token(INTEGER, int(current_char))
-            self.pos += 1
-            return token
+            if self.current_char == '+':
+                self.advance()
+                return Token(PLUS, '+')
 
-        if current_char == '+':
-            token = Token(PLUS, current_char)
-            self.pos += 1
-            return token
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
 
-        if current_char == '-':
-            token = Token(MINUS, current_char)
-            self.pos += 1
-            return token
+            self.error()
 
-        self.error()
+        return Token(EOF, None)
 
     def eat(self, token_type):
         # compare the current token type with the passed token
@@ -96,31 +96,36 @@ class Interpreter(object):
             self.error()
 
     def expr(self):
-        """expr -> INTEGER PLUS INTEGER"""
+        """Parser / Interpreter
+
+        expr -> INTEGER PLUS INTEGER
+        expr -> INTEGER MINUS INTEGER
+        """
         # set current token to the first token taken from the input
         self.current_token = self.get_next_token()
 
-        # we expect the current token to be a single-digit integer
+        # we expect the current token to be an integer
         left = self.current_token
         self.eat(INTEGER)
 
-        # we expect the current token to be a '+/-' token
+        # we expect the current token to be either a '+' or '-'
         op = self.current_token
         if op.type == PLUS:
             self.eat(PLUS)
         else:
             self.eat(MINUS)
 
-        # We expect the current token to be a single-digit integer
+        # we expect the current token to be an integer
         right = self.current_token
         self.eat(INTEGER)
         # after the above call the self.current_token is set to
         # EOF token
         
-        # at this point INTEGER PLUS/MINUS INTEGER sequence of tokens
+        # at this point either the INTEGER PLUS INTEGER or
+        # the INTEGER MINUS INTEGER sequence of tokens
         # has been successfully found and the method can just
-        # return the result of adding two integers, thus
-        # effectively interpreting client input
+        # return the result of adding or subtracting two integers,
+        # thus effectively interpreting client input
         if op.type == PLUS:
             result = left.value + right.value
         else:
